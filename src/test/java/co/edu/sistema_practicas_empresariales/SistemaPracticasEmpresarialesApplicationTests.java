bf165598675f5f32f461617a63b8aef22894ec38
samuel gomez <sgomez_21@cue.edu.co>
2026-06-03 13:40:24 -0500

    test(context): habilitar perfiles de prueba en aplicacion

--- src/test/java/co/edu/sistema_practicas_empresariales/SistemaPracticasEmpresarialesApplicationTests.java
+++ src/test/java/co/edu/sistema_practicas_empresariales/SistemaPracticasEmpresarialesApplicationTests.java
@@ -2,8 +2,10 @@ package co.edu.sistema_practicas_empresariales;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SistemaPracticasEmpresarialesApplicationTests {
 
     @Test

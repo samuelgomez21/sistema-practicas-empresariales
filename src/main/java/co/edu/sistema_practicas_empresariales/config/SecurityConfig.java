2a7f3285c13623aad01b6692846be428866f8c92
samuel gomez <sgomez_21@cue.edu.co>
2026-06-04 00:48:53 -0500

    fix(security): Revert God Mode and enable legal security constraints. Add DataSeeder for full Postman tests execution.

--- src/main/java/co/edu/sistema_practicas_empresariales/config/SecurityConfig.java
+++ src/main/java/co/edu/sistema_practicas_empresariales/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/api-docs/**").permitAll()
+                .requestMatchers("/error").permitAll()
                 .anyRequest().authenticated()
             );
 

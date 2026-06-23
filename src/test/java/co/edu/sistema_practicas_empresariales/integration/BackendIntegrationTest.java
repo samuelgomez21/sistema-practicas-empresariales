d6ff745462755804073f06fc2683eb007121d50b
samuel gomez <sgomez_21@cue.edu.co>
2026-06-22 22:11:36 -0500

    test: remove unused import in BackendIntegrationTest for zero warnings

--- src/test/java/co/edu/sistema_practicas_empresariales/integration/BackendIntegrationTest.java
+++ src/test/java/co/edu/sistema_practicas_empresariales/integration/BackendIntegrationTest.java
@@ -8,8 +8,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
-
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
